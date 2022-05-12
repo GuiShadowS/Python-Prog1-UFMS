d16419cbf00ee95df86b87b5caab576537a0bddd
Paulo Guilherme de Oliveira <driverblink@hotmail.com>
2022-05-12 03:12:59 -0400

    12/05 03:12

--- uri 2407 P1.py
+++ uri 2407 P1.py
@@ -13,40 +13,45 @@ esgoto = 0
 # o valor recebido e verificar em qual situação ele se encaixa
 if x > 50:
     agua = ((10 * 6.08) + (5 * 7.79) + (5 * 7.93) + (5 * 8.75) + (5 * 10.76) + (10 * 12.9) + ((x - 50) * 14.21))
-    esgoto = ((10 * 4.26) + (5 * 5.45) + (5 * 5.55) + (5 * 6.13) + (5 * 7.53) + (10 * 9.03) + ((x - 50) * 9.95))  
+    esgoto = ((10 * 4.26) + (5 * 5.45) + (5 * 5.55) + (5 * 6.13) + (5 * 7.53) + (10 * 9.03) + ((x - 50) * 9.95))
+    soma = 15.05 + agua + esgoto
         
 elif x >= 31 and x <= 50:
     agua = ((10 * 6.08) + (5 * 7.79) + (5 * 7.93) + (5 * 8.75) + (5 * 10.76) + ((x - 30) * 12.9))
     esgoto = ((10 * 4.26) + (5 * 5.45) + (5 * 5.55) + (5 * 6.13) + (5 * 7.53) + ((x - 30)* 9.03))
+    soma = 15.05 + agua + esgoto
            
 elif x >= 26 and x <= 30:
     agua = ((10 * 6.08) + (5 * 7.79) + (5 * 7.93) + (5 * 8.75) + ((x - 25) * 10.76)) 
     esgoto = ((10 * 4.26) + (5 * 5.45) + (5 * 5.55) + (5 * 6.13) + ((x - 25) * 7.53))
+    soma = 15.05 + agua + esgoto
            
 elif x >= 21 and x <= 25:
     agua = ((10 * 6.08) + (5 * 7.79) + (5 * 7.93) + ((x - 20) * 8.75))
     esgoto = ((10 * 4.26) + (5 * 5.45) + (5 * 5.55) + ((x - 20) * 6.13))
+    soma = 15.05 + agua + esgoto
            
 elif x >= 16 and x <= 20:
     agua = ((10 * 6.08) + (5 * 7.79) + ((x - 15) * 7.93)) 
     esgoto = ((10 * 4.26) + (5 * 5.45) + ((x - 15) * 5.55))
+    soma = 15.05 + agua + esgoto
         
 elif x >= 11 and x <= 15:
     agua = ((10 * 6.08) + ((x - 10) * 7.79))
     esgoto = ((10 * 4.26) + ((x - 10) * 5.45))
+    soma = 15.05 + agua + esgoto
 
-else:
+elif x >= 1 and x <= 10:
     agua = (x * 6.08) 
     esgoto = (x * 4.26)
+    soma = 15.05 + agua + esgoto
     
-# Após ja ter o valor de agua e esgoto, será calculado
-# o valor total a ser pago, somando com a taxa fixa de R$15.05
-soma = 15.05 + agua + esgoto
-    
+else:
+    soma = 15.05
+        
 # Agora será exibido na tela os prints conforme solicitado no exercicio
 # usando o "end" para concatenar as informações na mesma linha
-print("Consumo:",x, end=" ")
-print("metros cúbicos") 
+print("Consumo:",x,"metros cúbicos") 
 print("Total a pagar: R$ 15.05 + R$ {0:.2f}".format(agua),end=" ")
 print("+ R$ {0:.2f}".format(esgoto),end=" ")
 print("= R$ {0:.2f}".format(soma))
